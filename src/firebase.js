b2c412b95d3bc636e57a431f4f70685447ddb9bd
laxminarayan12 <51755071+laxminarayan12@users.noreply.github.com>
2022-02-09 20:44:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app";
 import "firebase/auth";
  
 export const  auth = firebase.initializeApp({
-    apiKey: "AIzaSyAxy-dBW8EI_CxDkNP-sZh-x6fnsPN72og",
+    apiKey: "API_ID",
     authDomain: "chat-app-3162a.firebaseapp.com",
     projectId: "chat-app-3162a",
     storageBucket: "chat-app-3162a.appspot.com",
     messagingSenderId: "521454226138",
-    appId: "1:521454226138:web:2aead01cc50c424198b3e3"
+    appId: "APP_ID"
   }).auth ();
